6f22a88f8c05e27410f47e25f2045cefc0946e01
Tom Fell <tomfell2360@gmail.com>
2021-01-27 09:05:41 +0000

    added the calculate velocities function within handle collisions. collisions are now (hopefully) dealt with - just need to find a way to show it!

--- code/collisions.py
+++ code/collisions.py
@@ -1,14 +1,17 @@
 import numpy as np
 import math 
 import random
+from itertools import combinations
 #not sure entirely how to do this without particle ball class - it will be different to the final class, so kept contained in this file 
 '''
-generates a load of particles that move around a box.
+generates a load of 2D particles that move around a 2D box, and sometimes smack each other.
 a lot of credit to:
 https://scipython.com/blog/two-dimensional-collisions/
 
 few other assumptions that will built in:
 all of the particles have the same density - of 1 - and so mass = volume 
+collisions are all perfectly elastic - momentum and kinetic energy conserved
+these govern the equations of motion used to handle all the collisions between particles
 '''
 class particle:
     '''
@@ -38,7 +41,15 @@ class sim:
         self.x_size = x_size
         self.y_size = y_size
 
+        self.get_particles(number_of_particles, radii)
+
+
     def get_particles(self, number, radii):
+        '''
+        'spawn' all the particles: number of particles and radii must be defined 
+        radii can either be a number, or a list of numbers that is right length - every particle needs a radii, and no extra! 
+
+        '''
 
         '''
         make sure the radius is in the form wanted
@@ -46,7 +57,7 @@ class sim:
 
         try: #check to see if radii is an array or a single number 
             radii.shape()
-            assert number == len(radii)
+            assert number == len(radii) #throws asserterror if the list isn't the right size
         except TypeError: #if it's a single number, make into a list of the same number 
             def radius_list_generator(number, radii):
                 for i in range(number):
@@ -73,5 +84,38 @@ class sim:
                      self.particles.append(new)
                      break
 
+
+    def collisions(self): 
+        '''
+        handles collsions between two particles - this is where the code diverts more from the example, so may be prone to breaking...
+
+        it was very prone to breaking so followed the example better! 
+
+        '''
+        
+        
+        def calculate_velocities(particle1,particle2):
+            '''
+            calculate the velocities after the collision 
+            '''
+            mass1, mass2 = particle1.radius ** 2, particle2.radius** 2 #assuming uniform density etc - mass is just the volume - or area in this case - pi not included for simplicity
+            totalMass = mass1 + mass2
+
+            pos1, pos2 = particle1.r, particle2.r
+            distance = np.linalg.norm(pos1 - pos2)**2 
+
+            v_before_1 = particle1.velocity
+            v_before_2 = particle2.velocity
+                
+            v_after_1 = v_before_1 - (2*mass2 / totalMass) * (np.dot(v_before_1-v_before_2, pos1- pos2) / distance) * (pos1- pos2)
+            v_after_2 = v_before_2 - (2*mass1 / totalMass) * (np.dot(v_before_2-v_before_1, pos2-pos1) / distance) * (pos2 - pos1)
+                
+            particle1.velocity = v_after_1
+            particle2.velocity = v_after_2
+                    
+        pairs = combinations(range(self.number),2) #need to check all possible particles
+        for i,j in pairs:
+            if self.particles[i].overlap(self.particles[j]): #check if they overlap
+                calculate_velocities(self.particles[i], self.particles[j]) #if they do overlap, then calculate the change in velocity
 
 
