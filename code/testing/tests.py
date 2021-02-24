036a245265aac56db2ea83547cb8b90b612fea1f
Tom Fell <tomfell2360@gmail.com>
2021-02-24 12:10:07 +0000

    added ideas to testing

--- code/testing/tests.py
+++ code/testing/tests.py
@@ -1,1 +1,6 @@
 ''' this file, and the folder it is contained in, will be used to test the code, using a testing suite such as PyTest '''
+
+''' IDEAS! 
+CHECK KE + PE = TOTAL E 
+CHECK STARTING E = INITIAL E
+'''
