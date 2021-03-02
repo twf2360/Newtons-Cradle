911dd714f7f8246f9e4bdb015b7ce5d57fb071ad
Tom Fell <tomfell2360@gmail.com>
2021-03-02 13:54:20 +0000

    added more config tests

--- code/tests.py
+++ code/tests.py
@@ -20,6 +20,10 @@ import json
 import dataframes
 import plotter
 
+'''
+The tests below are for the ball class. They test that a ball is initialised with the correct parameters, and that potential energy and kinetic energy are correctly calculated
+'''
+
 def ball_initialisation(position, velocity, radius, mass, anchor):
     test_ball = ball(position, velocity,radius,mass,anchor )
     return test_ball
@@ -46,12 +50,18 @@ def test_ball_initalisation_anchor():
     ball = ball_initialisation(position=[0,0], velocity=[0,0],radius= 1, mass = 1,anchor= [0,1])
     assert (ball.anchor == [0,1]).all()
 
+''' ADD KE AND PE HERE ''' 
+
+'''
+the next group of tests are for the the configuration file. This ensures that there is a dictionary that defines the configuration, and that it contains all of the needed parameters 
+
+'''
 def inital_config():
     with open(r"code\config.json") as configuration:
         config = json.load(configuration)
     return config
 
-def ensure_dict():
+def test_ensure_dict():
     config = inital_config()
     assert type(config) == dict
 
@@ -62,4 +72,12 @@ def test_for_initialisation_config():
 def test_for_system_config():
     config = inital_config()
     assert type(config['system']) == dict
+
+def test_keys_exist():
+    config = inital_config()
+    system = config['system']
+    initial = config['initialisation']
+    assert all(key in initial for key in(('iterations', 'number', 'StartPositions', 'StartVelocities', 'masses', 'radii', 'anchors'))) , "missing key for initialisation config"
+    assert all(key in system for key in('timesteps', 'approximations', 'densities')) , "missing key for system config"
+
 
