071211e0d3ff422da6b31ec0ed026c4d6d3109b2
Tom Fell <tomfell2360@gmail.com>
2021-03-13 11:37:38 +0000

    added two basic two balls tests

--- code/plotter_tests.py
+++ code/plotter_tests.py
@@ -56,6 +56,9 @@ def test_list_lengths():
     
 
 def test_energy_addition():
+    '''
+    test to ensure that the when adding kinetic and potential energy to get total energy, that the addition is done correctly
+    '''
     ''' use is close due to errors in floating point maths'''
     energies = plotter_init()
     ke = energies[0]
@@ -65,6 +68,33 @@ def test_energy_addition():
     random_time = 50000 * random()
     random_time_int = math.floor(random_time)
     assert (np.isclose(total[random_time_int],  (np.add(ke[random_time_int]  ,pe[random_time_int])))).all(), "total energy does not equal potential plus kinetic at the a random point"
+
+
+def two_ball_init():
+    '''
+    some of the results rely on the tota energy of the system, and therefore this is to check that these things are caluclated correctly with a more complicated system
+    '''
+    theta = math.pi/6 #initial starting angle! 
+    get_results = calculator(0.0001,50000)
+    get_results.get_balls(number = 2,positions= [[1 * math.sin(theta), -1 * math.cos(theta)], [0,-0.4]], velocities= [[0,0], [0,0]], radii=[0.02,0.02], masses=[1,1], anchors= [[0,0], [0.4]])
+    get_results.calculate(approximation='rk2', density=0)
+    plot = plotter('system_states_over_time', 2)
+    return plot
+
+def test_two_ball_energy():
+    '''
+    testing that the total energy of the system is calculated correctly - kinetic plus potential. 
+    '''
+    plot = two_ball_init()
+    ke = plot.total_kinetic_energy 
+    pe = plot.total_potential_energy
+    total = plot.total_energy
+    
+    ke_plus_pe = ke + pe 
+    assert np.isclose(ke_plus_pe[0], total[0]).all(), "total energy not equal to kinetic plus potential at start" 
+    random_time = 50000 * random()
+    random_time_int = math.floor(random_time)
+    assert np.isclose(ke_plus_pe[random_time_int], total[random_time_int0]).all(), "total energy not equal to kinetic plus potential at random time"
 
 '''
 this is more analysis than a test, really 
